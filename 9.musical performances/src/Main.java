733d4715144f6215145b0d6692fa9d21f3d2f210
lukezzz <patalee2001@gmail.com>
2023-04-04 21:15:15 +0530

    edited

--- 9.musical performances/src/Main.java
+++ 9.musical performances/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        var show1 = new LivePerformance(new MainArtist("Taylor Swift"),"Reputation",2023,"Glendale");
+        var show1 = new LivePerformance(new MainArtist("Taylor Swift"),"Eras Tour",2023,"Glendale");
         show1.setTrackList(new MusicTrack("Lavender Haze", 3.0f));
         show1.setTrackList(new MusicTrack("All Too Well", 2.45f));
         show1.setTrackList(new MusicTrack("the lakes", 3.45f));
--- 9.musical performances/src/StudioPerformance.java
+++ 9.musical performances/src/StudioPerformance.java
@@ -2,8 +2,14 @@ public class StudioPerformance extends Performance {
     public StudioPerformance(MainArtist mainArtist, String performanceName, int year, String venue) {
         super(mainArtist,performanceName,year,venue);
     } 
+    @Override
     public void record(){
         System.out.println("This is the method record in the  class StudioPerformance \n (overidden the medthod form abstract class performance)" ); 
         System.out.println("\nRecording in the studio\n");
+    }
+
+    public void beginAudioProcess(){
+        System.out.println("This is the method beginAudioProcess in the  class StudioPerformance" ); 
+        System.out.println("\nProcessing Audio\n");
     }
 }
