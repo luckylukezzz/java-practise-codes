1e71f85f401aed809774598a77a354b6b0d5ccc9
luckylukezzz <patalee2001@gmail.com>
2023-07-04 16:17:25 +0530

    ADDED

--- 21.popcorn machine/src/App.java
+++ 21.popcorn machine/src/App.java
@@ -16,14 +16,38 @@ class PopcornMachine{
         bagDone++;
         notifyAll();
     }
-    public synchronized void robotArm(){
+    public synchronized void robotArm(Bell bell, Light light){
         while(!(bagDone == 3)){
             try{wait();}catch(Exception e){};
         }
-        System.out.println("picked 1 BAG");
+        System.out.println("picked  BAG");
+        // Create threads for the asynchronous events
+        Thread bellThread = new Thread(bell::ring);
+        Thread lightThread = new Thread(light::blink);
 
+        // Start the event threads
+        bellThread.start();
+        lightThread.start();
+
+        //Wait for the event threads to complete
+        try {
+            bellThread.join();
+            lightThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+}
+class Bell {
+    public void ring() {
+        System.out.println("Bell rings");
+    }
+}
 
+class Light {
+    public void blink() {
+        System.out.println("Light blinks");
+    }
 }
 
 class Producer implements Runnable{
@@ -46,24 +70,28 @@ class Consumer1 implements Runnable{
 }
 class Consumer2 implements Runnable{
     PopcornMachine popcorn;
-    public Consumer2(PopcornMachine popcorn){
+    Bell bell;
+    Light light;
+    public Consumer2(PopcornMachine popcorn, Bell bell, Light light){
         this.popcorn = popcorn;
+        this.bell = bell;
+        this.light = light;
     }
     public void run(){
-        for (int i=0; i<3 ; i++) {
-        popcorn.robotArm();}
+        popcorn.robotArm(bell,light);
     }
 }
 public class App{
     public static void main (String[] args){
 
         PopcornMachine popcorn = new PopcornMachine();
+        
         Thread t1 = new Thread(new Producer(popcorn));
         Thread t2 = new Thread(new Producer(popcorn));
         Thread t3 = new Thread(new Consumer1(popcorn));
         Thread t4 = new Thread(new Consumer1(popcorn));
         Thread t5 = new Thread(new Consumer1(popcorn));
-        Thread t6 = new Thread(new Consumer2(popcorn));
+        Thread t6 = new Thread(new Consumer2(popcorn,new Bell(),new Light() ));
         t1.start();
         t2.start();
         t3.start();
